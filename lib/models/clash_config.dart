7379893c75d0dabe07a77e5955be304ab7d9f954
keeejiii <keeejiii@users.noreply.github.com>
2026-05-31 16:49:26 +0800

    fix(android): complete tun mtu model wiring

--- lib/models/clash_config.dart
+++ lib/models/clash_config.dart
@@ -210,6 +210,7 @@ abstract class Tun with _$Tun {
     @Default(appName) String device,
     @JsonKey(name: 'auto-route') @Default(false) bool autoRoute,
     @Default(TunStack.mixed) TunStack stack,
+    @Default(4064) int mtu,
     @JsonKey(name: 'dns-hijack') @Default(['any:53']) List<String> dnsHijack,
     @JsonKey(name: 'route-address') @Default([]) List<String> routeAddress,
   }) = _Tun;
